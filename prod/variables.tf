a05391c0407bddb938d2bde41e57365a7c17d270
azong B <azongbauclaire@gmail.com>
2025-10-18 16:40:16 -0400

    prod modified

--- prod/variables.tf
+++ prod/variables.tf
@@ -85,13 +85,13 @@ variable "key_name" {
 variable "domain_name" {
   description = "Primary domain name for certificate issuance"
   type        = string
-  default     = "azongtech"
+  default     = "azongtech.org"
 }
 
 variable "san_domains" {
   description = "SANs (Subject Alternative Names) for SSL certificate"
   type        = list(string)
-  default     = ["azongtech"]
+  default     = ["azongtech.org"]
 }
 
 variable "route53_zone_id" {
