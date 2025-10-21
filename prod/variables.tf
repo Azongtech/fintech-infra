2208bf249722153378c16cd961bdb1be68f998dc
azong B <azongbauclaire@gmail.com>
2025-10-21 03:33:33 -0400

    new variabl ecommit

--- prod/variables.tf
+++ prod/variables.tf
@@ -103,7 +103,7 @@ resource "aws_route53_record" "cert_validation" {
     for dvo in aws_acm_certificate.azongtech_cert.domain_validation_options : dvo.domain_name => dvo
   }
 
-  zone_id = "Z04206282XXO0JKCL1N69" # Replace with your actual hosted zone ID
+  zone_id = "Z09989351RBJT7ORQE7XL" # Replace with your actual hosted zone ID
   name    = each.value.resource_record_name
   type    = each.value.resource_record_type
   ttl     = 300
@@ -133,7 +133,7 @@ variable "san_domains" {
 variable "route53_zone_id" {
   description = "The ID of the Route53 hosted zone for the domain"
   type        = string
-  default     = "Z04206282XXO0JKCL1N69"
+  default     = "Z09989351RBJT7ORQE7XL"
 }
 
 
